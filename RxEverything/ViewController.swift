8ce8cfdb205414197fb53c3f34bb1cbbeee6e5d5
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-11-19 19:49:19 +0900

    TableView Button Add, Setting

--- RxEverything/ViewController.swift
+++ RxEverything/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import ThirdPartyLibraryManager
+import RxSwift
+import RxCocoa
+import SnapKit
+
 
 class ViewController: UIViewController {
-
+    
+    let goTableView : UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.setTitle("TableView의 숲", for: .normal)
+        
+        return button
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setView()
+        setConstraints()
+        bind()
+        
+    }
+    
+    func bind() {
+        
+    }
+    
+    func setView() {
+        self.view.addSubview(goTableView)
+    }
+    
+    func setConstraints(){
+        goTableView.snp.makeConstraints { make in
+            make.top.equalTo(200)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(200)
+            make.height.equalTo(50)
+        }
     }
 
 
 }
 
+    
